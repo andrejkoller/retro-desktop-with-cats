8d4bf3e9eebc186efa9f8a7e43bc9c788c7fcced
andrejkoller <andrejkoller@outlook.com>
2025-04-15 22:44:20 +0200

    Implement dynamic clock display in footer

--- src/js/main.js
+++ src/js/main.js
@@ -26,5 +26,43 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   };
 
+  const clock = () => {
+    let today = new Date();
+
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+
+    let meridiem;
+
+    if (hours >= 12) {
+      meridiem = " PM";
+    } else {
+      meridiem = " AM";
+    }
+
+    if (hours > 12) {
+      hours = hours - 12;
+    } else if (hours === 0) {
+      hours = 12;
+    }
+
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    } else {
+      minutes = minutes;
+    }
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    } else {
+      seconds = seconds;
+    }
+
+    document.getElementById("clock").innerHTML =
+      hours + ":" + minutes + ":" + seconds + meridiem;
+  };
+
   initDialogs();
+  setInterval(clock, 1000);
 });
